e0ec82eae06b21a86c74e6bb42df22a2b5e3f2a2
Ryan <ryan.manzer@gmail.com>
2019-09-22 08:43:16 -0700

    Building out basic nav bar styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+// Imports
 import { Component } from '@angular/core';
-
+// Component Decorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+// Component Logic
 export class AppComponent {
   title = 'AngYT';
 }
