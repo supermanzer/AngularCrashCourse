d21f0caf8a0f1986c76119bb261b71068100f619
Ryan <ryan.manzer@gmail.com>
2019-09-22 09:11:03 -0700

    Added home and list components and began working with one and two way data bindings as well as basic ng-template features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// We need to import our components here to allow us to create routes for them
+import { HomeComponent} from "./home/home.component";
+import { ListComponent} from "./list/list.component";
 
-const routes: Routes = [];
+
+// This array defines the routes or links we can use in our app to navigate around.
+const routes: Routes = [
+  {path: '', component: HomeComponent}, // This defines our first component
+  {path: 'list', component: ListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
