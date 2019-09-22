00972950f84ccab95940a35660920e996aa65f01
Ryan <ryan.manzer@gmail.com>
2019-09-22 09:20:30 -0700

    Revised style rules to use ngClass assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
-import { FormsModule} from "@angular/forms";
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
